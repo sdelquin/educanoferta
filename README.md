c1a8976c8cbf49965aff8e89fb0d01c6a2fe1485
Sergio Delgado Quintero <sdelquin@gmail.com>
2023-07-19 18:55:24 +0100

    Add typer for CLI management

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # nombrame
+
 Notificación de los nombramientos excepcionales de la Consejería de Educación del Gobierno de Canarias
--- main.py
+++ main.py
@@ -1,6 +1,27 @@
+import logzero
+import typer
+
 from lib.appointments import Manager
 from lib.utils import init_logger
 
 logger = init_logger()
+app = typer.Typer(add_completion=False)
 
-manager = Manager()
+
+@app.command()
+def run(
+    loglevel: str = typer.Option(
+        'DEBUG', '--loglevel', '-l', help='Log level (debug, info, error)'
+    ),
+):
+    '''Nombrame: Notificación de nombramientos'''
+
+    logger.setLevel(getattr(logzero, loglevel.upper()))
+    manager = Manager()
+
+
+if __name__ == "__main__":
+    try:
+        app()
+    except Exception as err:
+        logger.exception(err)
